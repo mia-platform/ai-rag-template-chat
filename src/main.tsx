290362f10573601467d2e2d34fd064a070cfa418
Davide Bianchi <10374360+davidebianchi@users.noreply.github.com>
2024-12-23 17:58:08 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {IntlProvider} from 'react-intl'
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
 
 import App from './App.tsx'
